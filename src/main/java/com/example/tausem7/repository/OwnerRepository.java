22655bcf7c1f7963bcefcc922e4ba32ad8224457
brash <s25070@pjwstk.edu.pl>
2024-11-09 21:20:30 +0100

    reorganize files

--- src/main/java/com/example/tausem7/repository/OwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tausem7.repository;
-
-import com.example.tausem7.domain.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OwnerRepository extends JpaRepository<Owner, Long> {
-}
